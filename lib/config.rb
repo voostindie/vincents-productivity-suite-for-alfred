40bf3282cf62d0b8688bb56a05e58a6fd87d41f9
Vincent Oostindie <vincent@ulso.nl>
2018-08-19 10:00:57 +0200

    Allow state to be persisted across program runs.

--- lib/config.rb
+++ lib/config.rb
@@ -2,21 +2,60 @@ require 'yaml'
 
 class Config
 
-  def initialize(hash)
-    @areas = extract_areas(hash)
+  DEFAULT_CONFIG_FILE= '$HOME/.plsrc'.freeze
+
+  def initialize(config_hash, state_hash)
+    @areas = extract_areas(config_hash)
+    @state = state_hash
   end
 
-  def self.load(path = "5HOME/.plsrc")
+  def self.load(path = DEFAULT_CONFIG_FILE)
     raise "Couldn't read config from '#{path}'" unless File.readable?(path)
-    Config.new(YAML.load_file(path))
+    config = YAML.load_file(path)
+    file = state_file(path)
+    state = if File.readable?(file)
+              YAML.load_file(file)
+            else
+              {}
+            end
+    Config.new(config, state)
   end
 
-  def areas()
+  def self.save_state(config, path = DEFAULT_CONFIG_FILE)
+    File.open(state_file(path), 'w') do |file|
+      file.write config.state.to_yaml
+    end
+  end
+
+  def self.delete_state(path = DEFAULT_CONFIG_FILE)
+    file = state_file(path)
+    File.delete(file) if File.exist?(file)
+  end
+
+  def areas
     @areas.keys.sort
   end
 
   def area(name)
     @areas[name]
+  end
+
+  def active_area
+    raise "No valid area is active" unless @state[:area] && @areas.include?(@state[:area])
+    @areas[@state[:area]]
+  end
+
+  def set_area(name)
+    raise "Unknown area '#{name}'" unless @areas.include?(name)
+    @state[:area] = name
+  end
+
+  def self.state_file(path)
+    "#{path}.state"
+  end
+
+  def state
+    @state
   end
 
   private
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,32 +1,58 @@
 require 'spec_helper'
 require 'config'
 
-describe Config, "#load" do
-  context "with a missing configuration file" do
-    it "raises an error" do
+describe Config, '#load' do
+
+  TEST_CONFIG_FILE = 'spec/plsrc.yaml'.freeze
+
+  context 'with a missing configuration file' do
+    it 'raises an error' do
       expect {
         Config.load('MISSING_FILE')
       }.to raise_error(RuntimeError, /Couldn't read/)
     end
   end
 
-  context "with a full-blown sample configuration file" do
-    it "loads successfully" do
-      config = Config.load('spec/plsrc.yaml')
-      expect(config.areas.size).to be(2)
-      expect(config.areas.include? 'work').to be(true)
+  context 'with a full-blown sample configuration file' do
 
+    config = Config.load(TEST_CONFIG_FILE)
+
+    it 'loads successfully' do
       work_expected = {
-          key: 'work',
-          name: 'Work'
+        key: 'work',
+        name: 'Work'
       }
       expect(config.area('work')).to eq(work_expected)
 
       personal_expected = {
-          key: 'personal',
-          name: 'Personal Stuff'
+        key: 'personal',
+        name: 'Personal Stuff'
       }
       expect(config.area('personal')).to eq(personal_expected)
     end
+
+    it 'returns all areas listed in order, sorted on key' do
+      expect(config.areas).to eq ['personal', 'work']
+    end
+
+    it 'allows an existing area to be selected' do
+      config.set_area('work')
+      expect(config.active_area[:key]).to eq('work')
+    end
+
+    it 'allows state to be saved to disk' do
+      state = "#{TEST_CONFIG_FILE}.state"
+      expect(File.exist?(state)).to be(false)
+      config.set_area('work')
+      Config.save_state(config, TEST_CONFIG_FILE)
+      expect(File.exist?(state)).to be(true)
+
+      config2 = Config.load(TEST_CONFIG_FILE)
+      expect(config2.active_area[:key]).to eq('work')
+
+      Config.delete_state(TEST_CONFIG_FILE)
+      expect(File.exist?(state)).to be(false)
+    end
   end
+
 end
