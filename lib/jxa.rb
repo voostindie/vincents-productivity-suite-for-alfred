b9b75bf6bd80be6d5d2f97c9c5eeebcddad5e252
Vincent Oostindie <vincent@ulso.nl>
2018-08-20 18:27:22 +0200

    Fix JXA scripts to work under a directory with "'" in the name.

--- lib/jxa.rb
+++ lib/jxa.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'shellwords'
 
 # Runs macOS JavaScript Automation scripts from the 'jxa' directory.
 # Output is always assumed to be in JSON format, which is parsed and returned as the result.
@@ -16,10 +17,11 @@ module Jxa
     def execute(script, *args)
       script = File.join(@root, script) + '.js'
       raise "JXA script not found: #{script}" unless File.exist?(script)
-      command = ([script] + args).join(' ')
+      command = ([Shellwords.escape(script)] + args).join(' ')
       json = `#{command}`
       raise "JXA script execution failed: '#{command}'" unless $? == 0
       JSON.parse(json)
     end
+
   end
 end
